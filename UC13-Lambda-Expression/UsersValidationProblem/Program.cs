42ab31b42e3ed5acd3c3c46dfdbc30a879b10f94
Dishant Dilip Yashwante <113447476+dishant1996@users.noreply.github.com>
2022-11-27 21:34:27 +0530

    Using Lambda Expression

---
+++ UC13-Lambda-Expression/UsersValidationProblem/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace UserRegistrationProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Problems Statement Using REGEX");
+            //User FirstName
+            Console.WriteLine("Enter the firstname: ");
+            string FirstName = Console.ReadLine();
+            Console.WriteLine(UserValidation.CheckFirstName(FirstName));
+            //User LastName
+            Console.WriteLine("Enter the lastname: ");
+            string lastname = Console.ReadLine();
+            Console.WriteLine(UserValidation.CheckLastName(lastname));
+            //User Email
+            Console.WriteLine("Enter the email: ");
+            string email = Console.ReadLine();
+            Console.WriteLine(UserValidation.checkEmail(email));
+            //User PhoneNumber
+            Console.WriteLine("Enter the phonenumber: ");
+            string phonenumber = Console.ReadLine();
+            Console.WriteLine(UserValidation.checkMobileNumber(phonenumber));
+            //User Password
+            Console.WriteLine("Enter the password: ");
+            string password = Console.ReadLine();
+            Console.WriteLine(UserValidation.checkPassword(password));
+        }
+    }
+}
